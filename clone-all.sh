acec50653eefb99fb7929ad0864b3077ef124c83
Jaimie Garner <pacificnm@gmail.com>
2017-12-05 22:31:17 +0000

    first commit

---
+++ clone-all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git clone https://github.com/Vulcan-Alchemy-Systems/vulcanalchemy.org.git
+git clone https://github.com/Vulcan-Alchemy-Systems/vulcaniot.net.git
+git clone https://github.com/Vulcan-Alchemy-Systems/vulcaniot.com.git
+git clone https://github.com/Vulcan-Alchemy-Systems/vulcanmedical.org.git
+git clone https://github.com/Vulcan-Alchemy-Systems/vulcanalchemyonline.com.git
+git clone https://github.com/Vulcan-Alchemy-Systems/vulcanalchemy.biz.git
